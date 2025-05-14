f7897c899609805de1f812aedb52057d454c43d0
John Brittain <john.brittain@dtc.ox.ac.uk>
2025-05-14 19:55:36 +0100

    Visualise baseline predictions

--- vis/streamlit/app.py
+++ vis/streamlit/app.py
@@ -2,17 +2,27 @@ import os
 import json
 import random
 import numpy as np
+import pandas as pd
 import pydeck as pdk
 import colorcet as cc
 import streamlit as st
+
 from PIL import Image, ImageDraw
+from pathlib import Path
+from datetime import datetime
 
 st.set_page_config(layout="wide")
 st.title("🦟 Dengue Prediction Platform")
 
-st.warning("Development build — using surrogate or demo data only.")
+st.warning("WARNING - Development build — Do not rely on data.")
 
 # --- Sidebar Controls
+metrics = {
+    "Cases (from samples)": "cases_samples",
+    "Log cases (from samples)": "log_cases_samples",
+    "Cases (from quantiles)": "cases_quantiles",
+    "Log cases (from quantiles)": "log_cases_quantiles",
+}
 countries = {
     "Peru": "PER",
     "Chile": "CHL",
@@ -33,6 +43,9 @@ colormaps = {
     "Cool": cc.kbc,
 }
 
+metric_name = st.sidebar.selectbox("Metric:", list(metrics.keys()))
+metric = metrics[metric_name]
+
 country = st.sidebar.selectbox("Country:", list(countries.keys()))
 admin_level = st.sidebar.selectbox("Admin Level:", list(admin_levels.keys()))
 selected_basemap = st.sidebar.selectbox("Basemap style:", list(basemaps.keys()))
@@ -51,9 +64,54 @@ with open(geojson_path, "r") as f:
     geojson = json.load(f)
 
 # --- Load Real Model Data
-model_data = {
-    f["properties"]["shapeName"]: random.uniform(0, 1) for f in geojson["features"]
-}
+# Fixed data file: baseline predictions (for now)...
+match metric:
+    case 'cases_samples':
+        filename = Path("./data/INLA/Output/baseline_log_cases.pred_dt_2018_2021.csv")
+        data_transform = lambda df: np.exp(np.median(df['prediction'])) + 1
+        tooltip_text = "Samples: {data}"
+    case 'log_cases_samples':
+        filename = Path("./data/INLA/Output/baseline_log_cases.pred_dt_2018_2021.csv")
+        data_transform = lambda df: np.median(df['prediction'])
+        tooltip_text = "Log samples: {data}"
+    case 'cases_quantiles':
+        filename = Path("./data/INLA/Output/quantile_baseline_log_cases.pred_dt_2018_2021.csv")
+        data_transform = lambda df: np.exp(df[df['quantile'] == 0.5]['prediction'].values[-1]) + 1  # last value (should only be one)
+        tooltip_text = "Quantile (log): {data}"
+    case 'log_cases_quantiles':
+        filename = Path("./data/INLA/Output/quantile_baseline_log_cases.pred_dt_2018_2021.csv")
+        data_transform = lambda df: df[df['quantile'] == 0.5]['prediction'].values[-1]  # last value (should only be one)
+        tooltip_text = "Quantile (log): {data}"
+    case _:
+        raise RuntimeError(f"Unrecognised metric: {metric_name}")
+
+if st.session_state.get('filename', None) != filename:
+    with st.spinner("Loading data..."):
+        st.session_state.filename = filename
+        df = pd.read_csv(str(filename), sep=',')
+        if 'location' not in df.columns and 'PROVINCE' in df.columns:
+            df = df.rename(columns={'PROVINCE': 'location'})
+        if 'target_end_date' not in df.columns and 'MONTH' in df.columns and 'YEAR' in df.columns:
+            df['target_end_date'] = pd.to_datetime(df[['YEAR', 'MONTH']].assign(day=1)) + pd.offsets.MonthEnd(0)
+        df['target_end_date'] = pd.to_datetime(df['target_end_date'])
+        model_data = {
+            f["properties"]["shapeName"]: np.nan for f in geojson["features"]
+        }
+        st.session_state.df = df
+        st.session_state.model_data = model_data
+
+df = st.session_state.df
+model_data = st.session_state.model_data
+
+if st.session_state.get('metric', None) != metric:
+    st.session_state.metric = metric
+    df_provinces = df['location'].unique()
+    for prov in model_data.keys():
+        if prov in df_provinces:
+            df1 = df[df['location'] == prov]
+            most_recent_time = df1['target_end_date'].max()
+            df1 = df1[df1['target_end_date'] == most_recent_time]
+            model_data[prov] = data_transform(df1)
 
 
 # --- Assign scores and fill colors
@@ -96,11 +154,22 @@ def get_colormap():
         cmap = list(reversed(cmap))
     return cmap
 
-for feature in geojson["features"]:
-    name = feature["properties"]["shapeName"]
-    risk = model_data.get(name, 0)
-    feature["properties"]["risk"] = f"{risk:.2f}"
-    feature["properties"]["fill_color"] = get_color(risk, get_colormap(), alpha_value)
+min_data = np.nanmin(list(model_data.values()))
+max_data = np.nanmax(list(model_data.values()))
+if max_data > 0:
+    color_white = [255, 255, 255, alpha_value]
+    for feature in geojson["features"]:
+        name = feature["properties"]["shapeName"]
+        data = model_data.get(name, np.nan)
+        if np.isnan(data):
+            # white fill
+            feature["properties"]["data"] = "-"
+            feature["properties"]["fill_color"] = color_white
+        else:
+            # scale color to data
+            normalised_data = (data - min_data) / (max_data - min_data)
+            feature["properties"]["data"] = f"{data:.1f}"
+            feature["properties"]["fill_color"] = get_color(normalised_data, get_colormap(), alpha_value)
 
 # --- Select basemap
 view_by_country = {
@@ -127,7 +196,7 @@ layer = pdk.Layer(
 
 # --- Tooltip config
 tooltip = {
-    "html": "<b>{shapeName}</b><br>Risk: {risk}",
+    "html": f"<b>{{shapeName}}</b><br>{tooltip_text}",
     "style": {"backgroundColor": "white", "color": "black"},
 }
 
@@ -141,7 +210,14 @@ deck = pdk.Deck(
 st.pydeck_chart(deck)
 
 # --- Colorbar
-st.subheader("Color Legend")
 colorbar_image = draw_colorbar(get_colormap(), width=400, height=25)
-st.image(colorbar_image)
-st.caption("Low Risk ←→ High Risk")
+col1, col2, col3 = st.columns([1, 1, 1])
+with col1:
+    st.markdown(
+        f"<div style='text-align:right; margin:0; padding:0; font-size:14px;'>{min_data:.1f}</div>",
+        unsafe_allow_html=True,
+    )
+with col2:
+    st.image(colorbar_image, use_container_width=True)
+with col3:
+    st.caption(f"{max_data:.2}")
